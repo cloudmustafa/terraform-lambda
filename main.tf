8dff5a3f73cac03efe56f46d7e330e40f1ae1f03
Akhterul Mustafa <amustafa2020@gmail.com>
2020-02-24 07:22:23 -0600

    Moved iam role to a seperate file

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Simple AWS Lambda Terraform Example
+# Deploy AWS Lambda using Terraform
 # requires 'index.js' in the same directory
 # to test: run `terraform plan`
 # to deploy: run `terraform apply`
@@ -17,31 +17,11 @@ data "archive_file" "lambda_zip" {
     output_path   = "lambda_function.zip"
 }
 
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_lambda_function" "terraform_test_lambda" {
   filename         = "lambda_function.zip"
-  function_name    = "test_lambda"
+  function_name    = "terraform_test_lambda"
   role             = "${aws_iam_role.iam_for_lambda_tf.arn}"
   handler          = "index.handler"
   source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
   runtime          = "nodejs12.x"
-}
-
-resource "aws_iam_role" "iam_for_lambda_tf" {
-  name = "iam_for_lambda_tf"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
 }
